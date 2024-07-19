132b229a5e8881c81083faa727cb48a1b32dcfa0
devMuromi <devmuromi@gmail.com>
2024-07-19 18:42:09 +0900

    fix: env 읽어오기 수정

--- ifive_server/settings.py
+++ ifive_server/settings.py
@@ -11,14 +11,14 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import environ
-
-env = environ.Env(DEBUG=(bool, True))
+import environ, os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env(DEBUG=(bool, True))
+environ.Env.read_env(env_file=os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
