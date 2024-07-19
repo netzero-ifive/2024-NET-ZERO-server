a3111552c20396d8592f7727add8e61e615d5384
devMuromi <devmuromi@gmail.com>
2024-07-19 19:08:53 +0900

    feat: rds mysql 연결

--- ifive_server/settings.py
+++ ifive_server/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = "ifive_server.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": env("POSTGRES_NAME"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
+        "HOST": env("POSTGRES_HOST"),
+        "PORT": "3306",
     }
 }
 
