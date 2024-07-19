c2e679b81ac5a273f520f401d494fd4eb192f086
devMuromi <devmuromi@gmail.com>
2024-07-19 18:50:02 +0900

    feat: 알러지류 모델 추가

--- product/admin.py
+++ product/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Product, Qrcode
+from .models import Product, Qrcode, Allergen
 
 admin.site.register(Product)
 admin.site.register(Qrcode)
+admin.site.register(Allergen)
