e146c0e6a23f18ede32d50bdd5a335c81fd6b3db
devMuromi <devmuromi@gmail.com>
2024-07-20 08:05:41 +0900

    feat: product에 id 추가

--- product/serializers.py
+++ product/serializers.py
@@ -22,6 +22,7 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = [
+            "id",
             "name",
             "price",
             "image",
