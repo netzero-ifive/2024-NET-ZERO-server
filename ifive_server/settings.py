ee065329f1e5a00ac05a2fedeb12c52b65c181c8
devMuromi <devmuromi@gmail.com>
2024-07-19 18:16:56 +0900

    feat: Product 앱 등록, 제품, qr코드 모델 생성

--- ifive_server/settings.py
+++ ifive_server/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "product",
+    # 3rd party
     "storages",
 ]
 
--- product/models.py
+++ product/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Qrcode(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    data = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="qrcodes/")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.qrcode
